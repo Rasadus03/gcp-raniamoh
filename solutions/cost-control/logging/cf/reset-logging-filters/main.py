5bbc6c28fe4b7eb570c59d5a6e062f1dc5262e3f
Rasdaus03 <admin@raniamoh.altostrat.com>
2021-09-15 13:10:12 +0200

    logging cost control soln

---
+++ solutions/cost-control/logging/cf/reset-logging-filters/main.py
@@ -0,0 +1,30 @@
+import base64
+import json
+import os
+# Imports the Cloud Logging client library
+import google.cloud.logging
+
+
+
+def turn_on_logs(request):
+    # Instantiates a client
+    logging_client = google.cloud.logging.Client()
+
+    # Retrieves a Cloud Logging handler based on the environment
+    # you're running in and integrates the handler with the
+    # Python logging module. By default this captures all logs
+    # at INFO level and higher
+   # logging_client.get_default_handler()
+    #logging_client.setup_logging()
+
+    for loadedSink in logging_client.list_sinks():
+        print("Loaded sink name {} ".format(loadedSink.name)) 
+        if loadedSink.name == "_Default" :
+            sink = loadedSink
+
+    sink.reload()
+
+    sink.filter_ = "severity >= INFO"
+    print("Updated sink {}".format(sink.filter_))
+    sink.update(unique_writer_identity=True)
+    return f'Success!!'
