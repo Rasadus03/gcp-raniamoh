6f8e347c0cf2fd3859013f3ebeeb8b8249428b44
Amanda Tarafa Mas <atarafamas@google.com>
2021-11-15 16:55:24 +0000

    Improvements on previous code.

--- samples/workload-identity/net-core/hello-app/CloudDemo.MvcCore/Controllers/HomeController.cs
+++ samples/workload-identity/net-core/hello-app/CloudDemo.MvcCore/Controllers/HomeController.cs
@@ -19,94 +19,78 @@
 // under the License.
 //
 
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using CloudDemo.MvcCore.Models;
+using Google.Apis.Auth;
 using Google.Apis.Auth.OAuth2;
-using System.Net.Http;
-using System.Net.Http.Headers;
+using Google.Apis.IAMCredentials.v1;
+using Google.Apis.IAMCredentials.v1.Data;
+using Google.Apis.Services;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System.IdentityModel.Tokens.Jwt;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace CloudDemo.MvcCore.Controllers
 {
     public class HomeController : Controller
     {
-        static HttpClient httpClient = new HttpClient();
+        public class CustomPayload : JsonWebSignature.Payload
+        {
+            [Newtonsoft.Json.JsonProperty("email")]
+            public string Email { get; set; }
+        }
 
         public static async Task<string> LoadJwtToken()
         {
-            var credentials = GoogleCredential.GetApplicationDefault();
-            var cred = (ICredential)credentials;
-            var computeCred = (ComputeCredential)(credentials.UnderlyingCredential);
-            var idToken = await credentials.GetOidcTokenAsync(OidcTokenOptions.FromTargetAudience("http…//test.travix.com"));
-            var jwtidToken = await ((OidcToken)idToken).GetAccessTokenAsync();
-            var handler = new JwtSecurityTokenHandler();
-            var jsonToken = handler.ReadToken(jwtidToken);
-            var tokenS = jsonToken as JwtSecurityToken;
-            String[] tokenString = tokenS.Payload.SerializeToJson().Replace("\"", "").Split(",");
-            var account = Array.Find(tokenString, element => element.StartsWith("email:")).Split(":")[1];
-            var baseAddress = new Uri("https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/" + account + ":signJwt");
-            var accessToken = await cred.GetAccessTokenForRequestAsync("https://other-service.travix.com");
-            httpClient.DefaultRequestHeaders.AcceptCharset.Add(new StringWithQualityHeaderValue("utf-8"));
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            long now = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-            long exp = DateTimeOffset.Parse(computeCred.Token.IssuedUtc.AddSeconds(computeCred.Token.ExpiresInSeconds.Value).ToString()).ToUnixTimeSeconds();
-            var payloadDetails = new
+            ICredential credentials = await GoogleCredential.GetApplicationDefaultAsync();
+
+            IAMCredentialsService iamClient = new IAMCredentialsService(new BaseClientService.Initializer
             {
-                iss = "app1-gsa@raniamoh-playground.iam.gserviceaccount.com",
-                sub = "app1-gsa@raniamoh-playground.iam.gserviceaccount.com",
-                aud = "http://some-service.travix.com",
-                iat = now,
-                exp = exp,
-                scope = "https://www.googleapis.com/auth/cloud-platform"
-            };
-            var payload = new
+                HttpClientInitializer = credentials
+            });
+
+            var accessToken = await credentials.GetAccessTokenForRequestAsync("https://other-service.travix.com");
+            var accessTokenPayload = await JsonWebSignature.VerifySignedTokenAsync<CustomPayload>(accessToken);
+
+            SignJwtRequest signRequest = new SignJwtRequest
             {
-                payload = JsonConvert.SerializeObject(payloadDetails)
+                Payload = JsonConvert.SerializeObject(new
+                {
+                    iss = "app1-gsa@raniamoh-playground.iam.gserviceaccount.com",
+                    sub = "app1-gsa@raniamoh-playground.iam.gserviceaccount.com",
+                    aud = "http://some-service.travix.com",
+                    iat = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
+                    exp = accessTokenPayload.ExpirationTimeSeconds,
+                    scope = "https://www.googleapis.com/auth/cloud-platform"
+                })
             };
-            var body = new StringContent(JsonConvert.SerializeObject(payload), System.Text.Encoding.UTF8, "application/json");
+
+            SignJwtResponse signResponse = await iamClient.Projects.ServiceAccounts.SignJwt(signRequest, accessTokenPayload.Email).ExecuteAsync();
+
             string message = " =============================================";
-            message += System.Environment.NewLine;
+            message += Environment.NewLine;
             message += " Account is ";
-            message += account;
-            message += System.Environment.NewLine;
+            message += accessTokenPayload.Email;
+            message += Environment.NewLine;
             message += " =============================================";
-            message += System.Environment.NewLine;
+            message += Environment.NewLine;
             message += " access token is ";
             message += accessToken;
-            message += System.Environment.NewLine;
+            message += Environment.NewLine;
             message += " =============================================";
-            message += System.Environment.NewLine;
-            message += " id token is ";
-            message += jwtidToken;
-            message += System.Environment.NewLine;
+            message += Environment.NewLine;
+            message += " custom jwt token " + signRequest.Payload;
+            message += Environment.NewLine;
             message += " =============================================";
-            message += System.Environment.NewLine;
-            message += " jwt token url  "                 + baseAddress;
-            message += System.Environment.NewLine;
-            message += " =============================================";
-            message += System.Environment.NewLine;
-            message += " jwt token headers  " + httpClient.DefaultRequestHeaders.Authorization;
-            message += System.Environment.NewLine;
-            message += " =============================================";
-            message += System.Environment.NewLine;
-            message += " jwt token body  " + await body.ReadAsStringAsync();
-            var response = await httpClient.PostAsync(baseAddress, body);
-            message += System.Environment.NewLine;
-            message += " =============================================";
-            message += System.Environment.NewLine;
-            message += " jwt token is ";
-            message += await response.Content.ReadAsStringAsync();
-            message += System.Environment.NewLine;
+            message += Environment.NewLine;
+            message += " signed custom jwt token is ";
+            message += signResponse.SignedJwt;
+            message += Environment.NewLine;
             message += " =============================================";
             return message;
         }
-
 
         public static async Task<string> TryUrl(string url)
         {
@@ -116,7 +100,6 @@ namespace CloudDemo.MvcCore.Controllers
             var response = await httpClient.SendAsync(httpRequest, default).ConfigureAwait(false);
             return $"StatusCode: {response.StatusCode} for URL: {url}";
         }
-
 
         public async Task<ActionResult> Index()
         {
