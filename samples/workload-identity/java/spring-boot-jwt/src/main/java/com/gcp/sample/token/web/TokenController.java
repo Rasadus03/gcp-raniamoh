568fa8dfe236fa032bf0f48d1ddb7ef0b69bc93b
Rasadus03 <rasadus@gmail.com>
2021-11-16 19:48:31 +0100

    Refactor for the solution to use the sdks and add docs

--- samples/workload-identity/java/spring-boot-jwt/src/main/java/com/gcp/sample/token/web/TokenController.java
+++ samples/workload-identity/java/spring-boot-jwt/src/main/java/com/gcp/sample/token/web/TokenController.java
@@ -1,41 +1,19 @@
 
 package com.gcp.sample.token.web;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.google.api.client.json.webtoken.JsonWebSignature;
-import com.google.api.client.json.webtoken.JsonWebToken;
-import com.google.auth.oauth2.AccessToken;
+import com.google.api.gax.core.FixedCredentialsProvider;
 import com.google.auth.oauth2.ComputeEngineCredentials;
-import com.google.auth.oauth2.CredentialAccessBoundary;
-import com.google.auth.oauth2.CredentialAccessBoundary.AccessBoundaryRule.AvailabilityCondition;
-import com.google.auth.oauth2.DownscopedCredentials;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.IdToken;
 import com.google.auth.oauth2.IdTokenProvider;
-import com.google.auth.oauth2.JwtClaims;
-import com.google.auth.oauth2.TokenVerifier;
-import com.google.iam.v1.IamPolicyProto;
-import java.io.IOException;
-import java.io.StringWriter;
+import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
+import com.google.cloud.iam.credentials.v1.IamCredentialsSettings;
+import com.google.cloud.iam.credentials.v1.SignJwtRequest;
+import com.google.cloud.iam.credentials.v1.SignJwtResponse;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.entity.BasicHttpEntity;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.DefaultBHttpClientConnection;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.impl.conn.DefaultManagedHttpClientConnection;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -52,14 +30,21 @@ public final class TokenController {
    * @return the index view template with a simple message
    */
   @GetMapping("/")
-  public String helloWorld(Model model) throws Exception {
+  public String loadJwtToken(Model model) throws Exception {
     String message = "";
     try {
       GoogleCredentials credential = GoogleCredentials.getApplicationDefault();
       ComputeEngineCredentials cred = ((ComputeEngineCredentials) credential);
+      IamCredentialsSettings iamCredentialsSettings =
+          IamCredentialsSettings.newBuilder()
+              .setCredentialsProvider(FixedCredentialsProvider.create(credential))
+              .build();
+      IamCredentialsClient iamCredentialsClient = IamCredentialsClient.create(
+          iamCredentialsSettings);
+      String account = cred.getAccount();
       message += "\n =============================================";
       message += "\n Account is ";
-      message += cred.getAccount();
+      message += account;
       message += "\n =============================================";
       message += "\n Meta Data is";
       message += cred.getRequestMetadata();
@@ -72,46 +57,36 @@ public final class TokenController {
       options.add(IdTokenProvider.Option.FORMAT_FULL);
       options.add(IdTokenProvider.Option.LICENSES_TRUE);
       options.add(IdTokenProvider.Option.INCLUDE_EMAIL);
-      message += cred.idTokenWithAudience("http…//test.travix.com", options);
+      IdToken idToken = cred.idTokenWithAudience("http…//test.travix.com", options);
+      message += idToken;
       message += "\n =============================================";
-      try (CloseableHttpClient httpclient = HttpClientBuilder.create().build()) {
-        HttpPost httpPost = new HttpPost(
-            "https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/"
-                + cred.getAccount() + ":signJwt");
-        httpPost.setHeader("Authorization", "Bearer " + cred.getAccessToken().getTokenValue());
-        httpPost.setHeader("Content-Type", " application/json");
-        httpPost.setHeader("charset", "utf-8");
-        message += "\n jwt token url  " + httpPost.getURI();
-        message += "\n =============================================";
-        message += "\n jwt token headers  " + httpPost.getFirstHeader("Authorization");
-        JSONObject payload = new JSONObject();
-        JSONObject jwtDetails = new JSONObject();
-        jwtDetails.put("iss", "app1-gsa@raniamoh-playground.iam.gserviceaccount.com");
-        jwtDetails.put("sub", "app1-gsa@raniamoh-playground.iam.gserviceaccount.com");
-        jwtDetails.put("aud", "http://some-service.travix.com");
-        long now = new Date().getTime() / 1000;
-        long exp = cred.getAccessToken().getExpirationTime().getTime() / 1000;
-        jwtDetails.put("iat", now);
-        jwtDetails.put("exp", exp);
-        jwtDetails.put("scope", "https://www.googleapis.com/auth/cloud-platform");
-        payload.put("payload", jwtDetails.toString());
-        httpPost.setEntity(new StringEntity(payload.toString(), ContentType.APPLICATION_JSON));
-        message += "\n =============================================";
-        message += "\n jwt token body  " + EntityUtils.toString(httpPost.getEntity());
-        try (CloseableHttpResponse response1 = httpclient.execute(httpPost)) {
-          System.out.println(
-              response1.getStatusLine().getReasonPhrase() + " " + response1.getStatusLine()
-                  .getStatusCode());
-          message += "\n =============================================";
-          message += "\n jwt token is ";
-          message += EntityUtils.toString(response1.getEntity());
-        }
-      } catch (Exception ioException) {
-        ioException.printStackTrace();
-      }
-      finally {
-      }
+      SignJwtRequest jwtRequest;
+      SignJwtRequest.Builder jwtBuilder = SignJwtRequest.newBuilder();
+      // SA account assigned to the workload
+      jwtBuilder.addDelegates("projects/-/serviceAccounts/" + account);
+      // SA account assigned to the workload with serviceAccountTokenCreator
+      jwtBuilder.setName("projects/-/serviceAccounts/" + account);
+      JSONObject jwtDetails = new JSONObject();
+      jwtDetails.put("iss", "app1-gsa@raniamoh-playground.iam.gserviceaccount.com");
+      jwtDetails.put("sub", "app1-gsa@raniamoh-playground.iam.gserviceaccount.com");
+      jwtDetails.put("aud", "http://some-service.travix.com");
+      long now = new Date().getTime() / 1000;
+      long exp = cred.getAccessToken().getExpirationTime().getTime() / 1000;
+      jwtDetails.put("iat", now);
+      jwtDetails.put("exp", exp);
+      jwtDetails.put("scope", "https://www.googleapis.com/auth/cloud-platform");
+      jwtBuilder = jwtBuilder.setPayload(jwtDetails.toString());
+      jwtRequest = jwtBuilder.build();
+      message += "\n =============================================";
+      message += "\n jwt token body  " + jwtRequest;
+      SignJwtResponse signResponse = iamCredentialsClient.signJwt(jwtRequest);
+      message += "\n =============================================";
+      message += "\n jwt token body  " + signResponse.toString();
+      message += "\n =============================================";
+      message += "\n jwt token is ";
+      message += signResponse.getSignedJwt();
       model.addAttribute("message", message);
+      iamCredentialsClient.close();
       return "index";
     } finally {
 
